1f9a5f04edf7ca440eeaa8a544e6138faa2c2a0a
fabricio farias <fabriciofarias9118@gmail.com>
2023-03-11 01:55:12 -0300

    ultimos exercicio de laços

---
+++ Laços/exercicio4.py
@@ -0,0 +1,6 @@
+#tabuada do 0 ate 10
+
+for i in range(0,11):
+    for j in range(0,11):
+        print(f"{i} * {j} = {i*j}")
+    print("-"*20)
---
+++ Laços/exercicio5.py
@@ -0,0 +1,4 @@
+#numeros pares com for
+
+for i in range(2,2001,2):
+    print(i)
